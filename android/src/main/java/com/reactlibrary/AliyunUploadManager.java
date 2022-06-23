a2abe9cc7d633322e3eb0a298d3e459f0e7ad21c
qiaomengnan16 <qiaomengnan2020@outlook.com>
2022-06-23 21:58:21 +0800

    Update AliyunUploadManager.java

--- android/src/main/java/com/reactlibrary/AliyunUploadManager.java
+++ android/src/main/java/com/reactlibrary/AliyunUploadManager.java
@@ -140,25 +140,26 @@ public class AliyunUploadManager {
     public void asyncAppendObject(final ReactContext context,String bucketName,String objectKey,String uploadFilePath,ReadableMap options,final Promise promise) {
 
         // Content to file:// start
+        sourceFile = sourceFile.replace("file:///", "/");
         Uri selectedVideoUri = Uri.parse(uploadFilePath);
 
         // 1. content uri -> file path
         // 2. inputstream -> temp file path
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getCurrentActivity().getContentResolver().query(selectedVideoUri, proj, null, null, null);
-            if (cursor == null) uploadFilePath = selectedVideoUri.getPath();
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            uploadFilePath = cursor.getString(column_index);
-        } catch (Exception e) {
-            uploadFilePath = FileUtils.getFilePathFromURI(context.getCurrentActivity(), selectedVideoUri);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
+        //Cursor cursor = null;
+        //try {
+        //    String[] proj = {MediaStore.Images.Media.DATA};
+        //    cursor = context.getCurrentActivity().getContentResolver().query(selectedVideoUri, proj, null, null, null);
+        //    if (cursor == null) sourceFile = selectedVideoUri.getPath();
+        //    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        //    cursor.moveToFirst();
+        //    sourceFile = cursor.getString(column_index);
+        //} catch (Exception e) {
+        //    sourceFile = FileUtils.getFilePathFromURI(context.getCurrentActivity(), selectedVideoUri);
+        //} finally {
+        //    if (cursor != null) {
+        //        cursor.close();
+        //    }
+        //}
 
         AppendObjectRequest append = new AppendObjectRequest(bucketName, objectKey, uploadFilePath);
         ObjectMetadata metadata = new ObjectMetadata();
